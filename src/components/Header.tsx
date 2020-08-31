2b19ac03a3dce14fe3d739b0bd1ece5e080bcecd
Ingrid <guid_95@hotmail.com>
2020-08-31 12:07:30 +0200

    adds menu component to header with functionalities

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,27 +4,82 @@ import Language from '../assets/svgs/language';
 import Account from '../assets/svgs/account';
 import Wish from '../assets/svgs/wish';
 import Basket from '../assets/svgs/basket';
-import Menu from '../assets/svgs/menu';
+import MenuIcon from '../assets/svgs/menu';
+import Menu from './Menu';
 import './Header.scss';
 
-const Header = () => (
-  <div className="header-container">
-    <div className="top-row">
-      <img src={logo} alt="Zalando logo" />
-      <div className="nav-items">
-        <Language />
-        <Account />
-        <Wish />
-        <Basket />
+type MyState = {
+  openMenu: boolean;
+  activeGender: string;
+  shrinkMenuHeader: boolean;
+};
+
+class Header extends React.Component<{}, MyState> {
+  state: MyState = {
+    openMenu: true,
+    activeGender: 'WOMEN',
+    shrinkMenuHeader: false,
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (e: any) => {
+    const scrolled = e.target.scrollTop;
+    const { shrinkMenuHeader } = this.state;
+
+    if (scrolled > 50 && !shrinkMenuHeader) {
+      this.setState({
+        shrinkMenuHeader: true,
+      });
+    } else if (scrolled < 50 && shrinkMenuHeader) {
+      this.setState({
+        shrinkMenuHeader: false,
+      });
+    }
+  };
+
+  handleClick = () => {
+    this.setState((prevState) => ({ openMenu: !prevState.openMenu }));
+  };
+
+  handleGenderChange = (gender: string) => {
+    this.setState({ activeGender: gender });
+  };
+
+  render() {
+    const { openMenu, activeGender, shrinkMenuHeader } = this.state;
+    return (
+      <div className="header-container">
+        <div className="top-row">
+          <img src={logo} alt="Zalando logo" />
+          <div className="nav-items">
+            <Language />
+            <Account />
+            <Wish />
+            <Basket />
+          </div>
+        </div>
+        <div className="bottom-row">
+          <button type="button" className="open-menu" onClick={this.handleClick}>
+            <MenuIcon />
+          </button>
+          <div className="search">
+            Search
+          </div>
+        </div>
+        <div className={`lateral-menu-open-${openMenu}`} onScroll={this.handleScroll}>
+          <Menu
+            close={this.handleClick}
+            changeGender={this.handleGenderChange}
+            activeGender={activeGender}
+            shouldShrink={shrinkMenuHeader}
+          />
+        </div>
       </div>
-    </div>
-    <div className="bottom-row">
-      <Menu />
-      <div className="search">
-        Search
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Header;
