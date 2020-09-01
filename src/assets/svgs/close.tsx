5caa3f114229ad1330baff728985a89b915af7f6
Ingrid <guid_95@hotmail.com>
2020-09-01 18:41:41 +0200

    fixes svgs

--- src/assets/svgs/close.tsx
+++ src/assets/svgs/close.tsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export default () => (<svg id="icon_svg-close-white-small" viewBox="0 0 24 24"><path d="M13.099 12.038l3.977 3.978-1.06 1.06-3.978-3.977-3.977 3.977L7 16.016l3.977-3.978L7 8.061 8.06 7l3.978 3.977L16.016 7l1.06 1.06-3.977 3.978z" fill="#FFF" fillRule="evenodd" /></svg>);
+const close = () => (<svg id="icon_svg-close-white-small" viewBox="0 0 24 24"><path d="M13.099 12.038l3.977 3.978-1.06 1.06-3.978-3.977-3.977 3.977L7 16.016l3.977-3.978L7 8.061 8.06 7l3.978 3.977L16.016 7l1.06 1.06-3.977 3.978z" fill="#FFF" fillRule="evenodd" /></svg>);
+
+export default close;
