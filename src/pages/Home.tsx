fcf73b4b7661df8ea86d938184926a8449d5d007
Ingrid <guid_95@hotmail.com>
2020-09-14 17:53:25 +0200

    adds banner img

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import banner1 from '../constants/imgs/imgs';
 
-const Home = () => (<Header />);
+import './Home.scss';
+
+const Home = () => (
+  <div>
+    <Header />
+    <img src={banner1} alt="banner1" />
+  </div>
+);
 
 export default Home;
