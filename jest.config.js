318792896d47562792aa6b93d508d0ef2a5e3ed3
Ingrid <guid_95@hotmail.com>
2020-09-21 17:09:20 +0200

    config jest

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,16 @@
 module.exports = {
-  preset: 'ts-jest',
-  roots: [
-    '<rootDir>/src',
-  ],
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'json',
-  ],
-  moduleDirectories: [
-    'node_modules',
-  ],
-  setupFiles: [
-    './setupTests.js',
-  ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  preset: "ts-jest",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  moduleDirectories: ["node_modules"],
+  setupFiles: ["./setupTests.js"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest',
-    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/fileTransformer.js',
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/fileTransformer.js",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
 };
