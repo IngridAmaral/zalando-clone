f9e93854abbad1e5617f00d2eb3fa3d12c5d5fa6
Ingrid <guid_95@hotmail.com>
2020-10-02 21:51:12 +0200

    creates flags data

---
+++ src/Data/flags.js
@@ -0,0 +1,4 @@
+export const FLAGS = {
+    hasSustainabilityFlag: { flagText: 'Sustainability', color: '#fff', background: '#00875a' },
+    isNew: { flagText: 'New', color: '#1a1a1a', background: '#fff' },
+}
