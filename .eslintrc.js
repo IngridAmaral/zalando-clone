6f954bff6d20f4d5d8524f3461ac66d8b3cadbaf
Ingrid <guid_95@hotmail.com>
2020-10-26 10:01:54 +0100

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
   extends: [
     'eslint:recommended',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
@@ -27,6 +29,7 @@ module.exports = {
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'react/prop-types': 'off',
     'prettier/prettier': [
       'error',
       {
@@ -41,6 +44,7 @@ module.exports = {
         jest: true,
       },
       rules: {
+        '@typescript-eslint/no-empty-function': 'off',
         'import/no-extraneous-dependencies': [
           'error',
           { devDependencies: true },
