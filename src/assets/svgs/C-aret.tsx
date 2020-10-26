430ecb664c1113fe79a6b3400b46a9e0ae865c48
Ingrid <guid_95@hotmail.com>
2020-10-26 10:27:13 +0100

    renaming assest step 1/2

--- src/assets/svgs/C-aret.tsx
+++ src/assets/svgs/C-aret.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const caret = (): JSX.Element => (
+const Caret = (): JSX.Element => (
   <svg id="icon_svg-chevron-right-small" viewBox="0 0 24 24">
     <path
       d="M10 7.71l.704-.71 4.71 4.673-4.704 4.741-.71-.704 3.99-4.02z"
@@ -10,4 +10,4 @@ const caret = (): JSX.Element => (
   </svg>
 );
 
-export default caret;
+export default Caret;
