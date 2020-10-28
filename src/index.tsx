e5f3ac5901577a0f2d45c2fecd56713bda8a0a4d
Ingrid <guid_95@hotmail.com>
2020-10-28 23:41:42 +0100

    adds a provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { createServer } from './server';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+createServer();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
